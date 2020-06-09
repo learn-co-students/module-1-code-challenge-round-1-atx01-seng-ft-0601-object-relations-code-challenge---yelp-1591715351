bea06667bf94198d5d2d4aeaf97e078657445b4b
VenkatNan <nanditavenkat@gmail.com>
2020-06-09 14:56:10 -0500

    submission for deadline

--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,7 @@ aet = Customer.new("Best","Bet")
 aon = Customer.new("Chris","Kris")
 aioli = Customer.new("Chris","Hoop")
 
+
 bluh = Restaurant.new("Elmers")
 blob = Restaurant.new("Chris-py's")
 Blep = Restaurant.new("Rip,Tear")
@@ -18,6 +19,7 @@ cad2 = Review.new("Chris-py's","lorraine Pearl",3)
 cad3 = Review.new("Chris-py's","Best Bet",4)
 cad4 = Review.new("Chris-py's","Chris Hoop",5)
 cad5 = Review.new("Chris-py's","Fisrt lagst",3)
+
 cod = Review.new("Elmers","Glue eater",5)
 cod2 = Review.new("Elmers","Fisrt last",1)
 
